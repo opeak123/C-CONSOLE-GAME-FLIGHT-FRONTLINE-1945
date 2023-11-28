1645fa252904ea9e8e8f6545a02ed8dc712109da
opeak123 <opeak123@hanmail.net>
2023-11-28 22:12:58 +0900

    23-11-28 Console game  /  Main menu  / Episode 1

---
+++ Flight Frontline 1945/Flight Frontline 1945/1945.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "Flight Title.h"
+#include "Flight Init.h"
+
+
+int main() 
+{
+    
+    CONSOLE_INIT();
+    CONSOLE_TITLE();
+    INSERTMENU();
+
+
+
+
+
+
+    //콘솔 일시정지
+    //system("pause");
+    return 0;
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# C-Console-game
